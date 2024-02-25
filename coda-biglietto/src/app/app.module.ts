9abb632ab32f291c52d2c874b12b5890abed247d
Mr. S.M. <simomike2015@gmail.com>
2024-02-26 00:15:35 +0100

    adas

--- coda-biglietto/src/app/app.module.ts
+++ coda-biglietto/src/app/app.module.ts
@@ -8,12 +8,12 @@ import { AppComponent } from './app.component';
 import { BigliettoComponent } from './biglietto/biglietto.component';
 
 //ANGULAR MATERIAL
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [ BigliettoComponent,AppComponent],
